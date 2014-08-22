bd9cbecb50b862cf597f14a20519f7bd1d7cb47c
Pablo Ramos <pabloramos1987@gmail.com>
2014-08-21 23:47:22 -0300

    Nuevas vistas y pruebas

---
+++ src/com/frba/abclandia/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.frba.abclandia;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends Activity {
+	
+	protected void onCreate(Bundle paramBundle){
+		super.onCreate(paramBundle);
+		requestWindowFeature(1);
+		setContentView(R.layout.activity_login);
+	}
+	
+	public void loginMaestro(View view){
+		
+	}
+	
+	public void loginPadre (View view){
+		
+	}
+
+}
---
+++ src/com/frba/abclandia/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.frba.abclandia;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashActivity extends Activity {
+	private static final int DISPLAY = 3000;
+	
+	protected void callNextActivity(){
+		startActivity(new Intent(this, LoginActivity.class));
+	}
+	
+	protected void onCreate(Bundle paramBundle){
+		super.onCreate(paramBundle);
+		requestWindowFeature(1);
+		setContentView(R.layout.activity_splash);
+		new Handler().postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				SplashActivity.this.callNextActivity();
+				SplashActivity.this.finish();
+				
+			}
+		}, DISPLAY);
+	}
+}
